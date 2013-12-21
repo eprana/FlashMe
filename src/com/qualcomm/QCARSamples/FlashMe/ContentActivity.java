4603d78bca07bb62e7686c6f65e250c1781f6e02
rolfich <x__flow@hotmail.fr>
2013-12-21 17:09:32 +0100

    menu is working and beautifuuul

--- src/com/qualcomm/QCARSamples/FlashMe/ContentActivity.java
+++ src/com/qualcomm/QCARSamples/FlashMe/ContentActivity.java
@@ -1,5 +1,7 @@
 package com.qualcomm.QCARSamples.FlashMe;
 
+import java.util.ArrayList;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -8,34 +10,106 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ContentActivity extends FragmentActivity {
 
+  // -------------------- TO CHANGE WITH THE BDD (setting the username on top of the page)
+	// Data to get
+	final static String EXTRA_LOGIN = "user_login";
+  // ------------------------------------------------------------------------------------
+	
 	public static class ProfileFragment extends Fragment {
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
-		{
-			View mainView = inflater.inflate(R.layout.profile, container, false);		
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+			View mainView = inflater.inflate(R.layout.profile, container, false);	
+			
+		//	-------------------- TO CHANGE WITH THE BDD (setting the username on top of the page)
+        	//Intent intent = getIntent();
+        	TextView userName = (TextView) mainView.findViewById(R.id.name);
+	        //if(intent != null){
+	        	//userName.setText(intent.getStringExtra(EXTRA_LOGIN));
+        	userName.setText(EXTRA_LOGIN);
+	        //}
+	    // --------------------------------------------------------------------------------------
 			return mainView;
 		}
 	}
 	
 	public static class TeamsFragment extends Fragment {
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
-		{
-			View mainView = inflater.inflate(R.layout.teams, container, false);		
+
+		private ExpandableListView expandableList = null;
+	
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+			View mainView = inflater.inflate(R.layout.teams, container, false);
+			Context context = mainView.getContext();
+			
+			//	-------------------- TO CHANGE WITH THE BDD (setting the username on top of the page)
+        	//Intent intent = getIntent();
+        	TextView userName = (TextView) mainView.findViewById(R.id.name);
+	        //if(intent != null){
+	        	//userName.setText(intent.getStringExtra(EXTRA_LOGIN));
+        	userName.setText(EXTRA_LOGIN);
+	        //}
+	    // --------------------------------------------------------------------------------------
+			expandableList = (ExpandableListView) mainView.findViewById(R.id.teams_list);
+			ArrayList<Team> teams = new ArrayList<Team>();
+
+			//Teams tests
+			Team team1 = createTeam("Anti-Heroes", "Zizi");
+			team1.addPlayer(team1, "Zizi", getResources().getDrawable(R.drawable.pic_empty_mini));
+			team1.addPlayer(team1, "Flo", getResources().getDrawable(R.drawable.pic_flo));
+			team1.addPlayer(team1, "Xopi", getResources().getDrawable(R.drawable.pic_empty_mini));
+			team1.setReady(true);
+			
+			Team team2 = createTeam("Hydro-Gène", "Flo");
+			team2.addPlayer(team2, "Flo", getResources().getDrawable(R.drawable.pic_flo));
+			team2.addPlayer(team2, "Jiji", getResources().getDrawable(R.drawable.pic_empty_mini));
+			team2.addPlayer(team2, "Cédric", getResources().getDrawable(R.drawable.pic_empty_mini));
+			team2.setReady(true);
+
+			//Players are ready? tests
+			team1.getPlayers().get(0).setReady(true);
+			team1.getPlayers().get(2).setReady(true);
+			team2.getPlayers().get(0).setReady(true);
+			team2.getPlayers().get(1).setReady(true);
+			team2.getPlayers().get(2).setReady(true);
+			
+			//Changing picture tests
+			team1.getPlayers().get(2).setPicture(getResources().getDrawable(R.drawable.pic_xopi));
+					
+			//Add players to team
+			teams.add(team1);
+			teams.add(team2);
+			
+			ELVAdapter adapter = new ELVAdapter(context, teams);
+			expandableList.setAdapter(adapter);
+
 			return mainView;
+		}
+				
+		public Team createTeam(String name, String creator){
+			return new Team(name, creator);
 		}
 	}
 
 	public static class GamesFragment extends Fragment {
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
-		{
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 			View mainView = inflater.inflate(R.layout.games, container, false);		
+			//	-------------------- TO CHANGE WITH THE BDD (setting the username on top of the page)
+        	//Intent intent = getIntent();
+        	TextView userName = (TextView) mainView.findViewById(R.id.name);
+	        //if(intent != null){
+	        	//userName.setText(intent.getStringExtra(EXTRA_LOGIN));
+        	userName.setText(EXTRA_LOGIN);
+	        //}
+	    // --------------------------------------------------------------------------------------
 			return mainView;
 		}
 	}
@@ -53,13 +127,16 @@ public class ContentActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 	 	setContentView(R.layout.content);
 	 	
-	 	ImageButton profile_bt = (ImageButton) findViewById(R.id.profile_bt);
-	 	ImageButton teams_bt = (ImageButton) findViewById(R.id.team_bt);
-		ImageButton games_bt = (ImageButton) findViewById(R.id.game_bt);
+	 	final ImageButton profile_bt = (ImageButton) findViewById(R.id.profile_bt);
+	 	final ImageButton teams_bt = (ImageButton) findViewById(R.id.team_bt);
+		final ImageButton games_bt = (ImageButton) findViewById(R.id.game_bt);
 	 	
 		profile_bt.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View arg0){
+				profile_bt.setImageResource(R.drawable.profile_bt);
+				teams_bt.setImageResource(R.drawable.team_bt_in);
+				games_bt.setImageResource(R.drawable.game_bt_in);
 				ProfileFragment fragment = (ProfileFragment)myFragmentManager.findFragmentByTag(TAG_PROFILE);
 				if(fragment==null){
 					FragmentTransaction fragmentTransaction = myFragmentManager.beginTransaction();
@@ -72,6 +149,9 @@ public class ContentActivity extends FragmentActivity {
 		teams_bt.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View arg0){
+				profile_bt.setImageResource(R.drawable.profile_bt_in);
+				teams_bt.setImageResource(R.drawable.team_bt);
+				games_bt.setImageResource(R.drawable.game_bt_in);
 				TeamsFragment fragment = (TeamsFragment)myFragmentManager.findFragmentByTag(TAG_TEAMS);
 				if(fragment==null){
 					FragmentTransaction fragmentTransaction = myFragmentManager.beginTransaction();
@@ -84,6 +164,9 @@ public class ContentActivity extends FragmentActivity {
 		games_bt.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View arg0){
+				profile_bt.setImageResource(R.drawable.profile_bt_in);
+				teams_bt.setImageResource(R.drawable.team_bt_in);
+				games_bt.setImageResource(R.drawable.game_bt);
 				GamesFragment fragment = (GamesFragment)myFragmentManager.findFragmentByTag(TAG_GAMES);
 				if(fragment==null){
 					FragmentTransaction fragmentTransaction = myFragmentManager.beginTransaction();
