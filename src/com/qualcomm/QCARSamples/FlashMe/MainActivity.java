2518d655d6f16078e149f0a5a0e332ecff84e461
rolfich <x__flow@hotmail.fr>
2013-12-22 21:52:46 +0100

    adding the about popup on main screen

--- src/com/qualcomm/QCARSamples/FlashMe/MainActivity.java
+++ src/com/qualcomm/QCARSamples/FlashMe/MainActivity.java
@@ -6,12 +6,16 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Rect;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +27,9 @@ public class MainActivity extends Activity {
 
 	private final Context context = this;
 	private View alertDialogView;
+	private PopupWindow popup;
+	private ImageButton aboutButton = null;
+	private ImageButton closeButton = null;
 
 	// Data to get
 	private final String EXTRA_LOGIN = "user_login";
@@ -41,6 +48,15 @@ public class MainActivity extends Activity {
 		final EditText pass = (EditText) findViewById(R.id.password);
 		final Button logInButton = (Button) findViewById(R.id.login);
      	
+		aboutButton = (ImageButton) findViewById(R.id.bobble_help);
+		aboutButton.setOnClickListener(new OnClickListener() {
+
+			@Override 
+			public void onClick(View v) { 
+				initiatePopupWindow(aboutButton); 
+			} 
+		});
+		
 		logInButton.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -117,4 +133,43 @@ public class MainActivity extends Activity {
 			}
 		});
 	}		
+	
+	private void initiatePopupWindow(View parentView) { 
+		try { 
+			// Get the instance of the LayoutInflater 
+			LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+			View layout = inflater.inflate(R.layout.popup,(ViewGroup) findViewById(R.id.popup)); 
+			popup = new PopupWindow(layout, 600, 300, true); 
+			popup.showAtLocation(parentView, Gravity.TOP|Gravity.LEFT, locateView(aboutButton).left, locateView(aboutButton).bottom+10);
+		
+			closeButton = (ImageButton) layout.findViewById(R.id.close_popup); 
+			closeButton.setOnClickListener(new OnClickListener() { 
+				public void onClick(View v) { 
+					popup.dismiss();
+				} 
+			});
+		} catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+	} 
+	
+	public static Rect locateView(View v)
+	{
+	    int[] loc_int = new int[2];
+	    if (v == null) return null;
+	    try {
+	        v.getLocationOnScreen(loc_int);
+	    } catch (NullPointerException npe) {
+	        //Happens when the view doesn't exist on screen anymore.
+	        return null;
+	    }
+	    Rect location = new Rect();
+	    location.left = loc_int[0];
+	    location.top = loc_int[1];
+	    location.right = location.left + v.getWidth();
+	    location.bottom = location.top + v.getHeight();
+	    return location;
+	}
+	
+	
 }
