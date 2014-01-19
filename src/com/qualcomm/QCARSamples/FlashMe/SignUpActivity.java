f233e8a36678fb7d67fb433e7c186f54522c5612
rolfich <x__flow@hotmail.fr>
2014-01-19 18:28:22 +0100

    sending mail when singning up

--- src/com/qualcomm/QCARSamples/FlashMe/SignUpActivity.java
+++ src/com/qualcomm/QCARSamples/FlashMe/SignUpActivity.java
@@ -2,11 +2,23 @@ package com.qualcomm.QCARSamples.FlashMe;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -20,9 +32,12 @@ import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.text.Html;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -59,6 +74,9 @@ public class SignUpActivity extends Activity {
     private Bitmap bitmapToSent;
     private Bitmap bitmapMarkerToSent;
     private boolean hasBeenCreated = false;
+    private final static String applimail = "flashme.app@gmail.com";
+    private final static String applipass = "flashmeapp";
+    private static String useraddress;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -157,8 +175,9 @@ public class SignUpActivity extends Activity {
       	   			Toast.makeText(context, R.string.wrong_mail_pattern, Toast.LENGTH_SHORT).show();
       	   			return;
       	   		}
-				
-      	   		// If nothing is wrong, add new User to DataBase
+      	   		
+      	   		// If nothing is wrong, add new User to DataBase and sand e-mail
+      	   		useraddress = s_email;
              	newUser = new ParseUser();
              	newUser.setUsername(s_username);
              	newUser.setPassword(s_password);
@@ -169,7 +188,7 @@ public class SignUpActivity extends Activity {
              		public void done(ParseException e) {
              			if (e == null) {
              				// Notify the user his account has been created and that his marker will be sent by mail
-                  	   		// Create an alert box
+             				// Create an alert box
             				AlertDialog.Builder adb = new AlertDialog.Builder(context);
             				MessageAlert msg_a;
             				
@@ -195,6 +214,13 @@ public class SignUpActivity extends Activity {
             		            public void onClick(DialogInterface dialog, int which) {
             		            	hasBeenCreated = true;
             		            	Intent intent = new Intent(context, ContentActivity.class);
+                          	   		// Send an e-mail
+                     				
+                     				String email = useraddress;
+                                    String subject = "Welcome !";
+                                    String message = context.getResources().getString(R.string.email_to_send, s_username, s_password, "http://www.pouet.fr");
+                                    sendMail(email, subject, message);
+                                    
             		            	startActivityForResult(intent, CREATE_PROFILE);
             		        } });
             				
@@ -296,4 +322,70 @@ public class SignUpActivity extends Activity {
     		}else Toast.makeText(SignUpActivity.this, "No Photo Selected", Toast.LENGTH_SHORT).show();
     	}
     }
+    
+    private Session createSessionObject() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+     
+        return Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("flashme.app@gmail.com", "flashmeapp");
+            }
+        });
+    }
+    
+    private Message createMessage(String email, String subject, String messageBody, Session session) throws MessagingException, UnsupportedEncodingException {
+        Message message = new MimeMessage(session);
+        message.setContent(messageBody, "text/html");
+        message.setFrom(new InternetAddress("flashme.app@gmail.com", "Flash Me"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+    
+    private class SendMailTask extends AsyncTask<Message, Void, Void> {
+//        private ProgressDialog progressDialog;
+     
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            progressDialog = ProgressDialog.show(SignUpActivity.this, "Please wait", "Sending mail", true, false);
+        }
+     
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+//            progressDialog.dismiss();
+        }
+     
+        @Override
+        protected Void doInBackground(Message... messages) {
+            try {
+                Transport.send(messages[0]);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    
+    private void sendMail(String email, String subject, String messageBody) {
+        Session session = createSessionObject();
+     
+        try {
+            Message message = createMessage(email, subject, messageBody, session);
+            new SendMailTask().execute(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
